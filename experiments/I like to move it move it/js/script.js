551f73294c0d90dd558977fbf0868a7d5cd1adfa
thisissherachel <rebr998@gmail.com>
2021-09-20 23:08:26 -0400

    setting up assignment 01

---
+++ experiments/I like to move it move it/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+I like to move it move it
+Rachel B. Richard
+
+ASSIGNMENT 01
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
