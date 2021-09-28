162d5c3ca1f1c96f43fef367cbb06a62555e678c
thisissherachel <rebr998@gmail.com>
2021-09-28 12:15:05 -0400

    creating an activity folder

---
+++ activities/dodging Covid-19/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+Dodging Covid19
+Rachel B. Richard
+
+ACTICITY 04
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
---
+++ activities/mona-lisa/js/script.js
@@ -0,0 +1,47 @@
+/**
+This is the Mona Lisa.
+Rachel B. Richard
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+function setup() {
+    createCanvas (240, 350);
+}
+
+function draw() {
+    //background
+    background(144, 191, 122);
+    //hair
+    fill(61, 54, 35);
+    ellipse(120, 90, 100, 120);
+    //face
+    noStroke();
+    fill(201, 175, 101);
+    ellipse(120, 85, 60, 80);
+    //body
+    noStroke();
+    fill(201, 175, 101);
+    ellipse(120, 220, 185, 200);
+    //clothes
+    fill(61, 54, 35);
+    rect(27, 180, 185, 200, 20, 20);
+    //smile
+    stroke(0);
+    line(100, 90, 120, 95);
+    line(140, 90, 120, 95);
+    //eyes
+    noStroke()
+    fill(61, 54, 35);
+    ellipse(128, 75, 5, 5);
+    ellipse(108, 75, 5, 5);     
+
+}
