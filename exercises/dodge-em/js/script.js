175fd0a8544c5d3438e5e1941eee1e076070e7de
thisissherachel <rebr998@gmail.com>
2021-09-28 14:05:38 -0400

    Setting up exercise 02 with my plan

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -4,48 +4,51 @@
 Dodge-em
 Rachel B. Richard
 
-ACTICITY 04
+EXERCISE 02
 
-1. Display the COVID-19 circle and move it across the screen, starting at a random y
-2. Make the COVID-19 circle move back to the left if it goes off the right side
-3. Display the user circle at the mouse location
-4. Check if the two circles overlap and, if they do, stop the program
-5. Display random static in the background for a visual flourish
-6. Hide the mouse cursor
+1. Change the way the user controls their circle
+2. Add at least one new if-statement (including at least an else)
+3. Change the way the simulation looks
+4. Use at least one image
+
+Program is about avoiding the negativity. You are happy until you touch the negativity object.
+The environment is happy with hearts until you touch object, it switches to darkness and sluggyness.
 
 **************************************************/
 
-let covid19 = { //creating the covid19 circle object
+let nagativity = { //creating the negativity object
   x: 0,
   y: 250,
   size: 100,
   vx: 0,
   vy: 0,
   speed: 5,
-  fill: { //creating the red fill property with object inside of object
-    r: 255,
-    g: 0,
-    b: 0,
   }
 };
 
-let user = { //creating the object fro the interactive mouse
+let user = { //creating the object for the interactive mouse
   x: 250,
   y: 250,
   size:100,
   fill: 100,
 };
 
-let numStatic = 1000; //creating variable for the display static
+
+//PRELOAD
+//for images and asets goign to be used in the running grogram
+function preload(){
+  negativity = loadImage('assets/images/negativity.jpg')
+  user = loadImage('assets/images/happy.jpg')
+}
 
 // SETUP()
 //for full program running setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  //setting up the covid19 settings
-  covid19.y = random(0,height); //moving at random y position
-  covid19.vx = covid19.speed; //moving horizontally with its speeed
+  //setting up the negativity settings
+  negativity.y = random(0,height); //moving at random y position
+  negativity.vx = negativity.speed; //moving horizontally with its speeed
 
   //removing cursor
   noCursor();
@@ -57,22 +60,21 @@ function setup() {
 function draw() {
   background(0);
 
-  //display static
-  for (let i = 0; i < numStatic; i++){ //a for loop saying it starts at 0, goes up to a number, and goes up by 1 ever time
-    //genrating a point randomly on the window
-    let x = random(0,width);
-    let y = random(0,height);
-    stroke(255);
-    point(x,y);
-  }
+  //negativity display
+  imageMode(CENTER);
+  image(negativity, width, height);
 
-  //covid19  movement
-  covid19.x += covid19.vx; //standard moving property
-  covid19.y += covid19.vy;
+  //user display
+  imageMode(CENTER);
+  image(user, width, height);
 
-  if (covid19.x > width) { //keep it in the width and continue the random y position
-    covid19.x = 0;
-    covid19.y = random(0,height);
+  //negativity  movement
+  negativity.x += negativity.vx; //standard moving property
+  negativity.y += negativity.vy;
+
+  if (negativity.x > width) { //keep it in the width and continue the random y position
+    negativity.x = 0;
+    negativity.y = random(0,height);
   }
 
   //user movement
@@ -80,14 +82,14 @@ function draw() {
   user.y = mouseY;
 
   //check for catching Covid19
-  let d = dist(user.x,user.y,covid19.x,covid19.y); //finding the distance between user and Covid19
-  if (d < covid19.size/2 + user.size/2) { //finding when they overlap
+  let d = dist(user.x,user.y,negativity.x,negativity.y); //finding the distance between user and Covid19
+  if (d < negativity.size/2 + user.size/2) { //finding when they overlap
     noLoop(); //stop the simulation once above is true
   }
 
-  //display covid19
-  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
-  ellipse(covid19.x,covid19.y,covid19.size);
+  //display negativity
+  fill(negativity.fill.r,negativity.fill.g,negativity.fill.b);
+  ellipse(negativity.x,negativity.y,negativity.size);
 
   //display user
   fill(user.fill);
