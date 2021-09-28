f76947e8663049e39ca1abe853c4c744c366a16c
thisissherachel <rebr998@gmail.com>
2021-09-28 12:22:07 -0400

    organizing exercises folder

---
+++ exercises/dodge-em/js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**************************************************
+Dodge-em
+Rachel B. Richard
+
+Assignment 02
+
+1. Change the way the user controls their circle
+2. Add at least one new if-statement
+3. Change the way the simulation looks
+4. Use at least one image
+**************************************************/
+
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
