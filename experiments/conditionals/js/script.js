25cb00e015452982b50848ee55b9791b4c40d91c
thisissherachel <rebr998@gmail.com>
2021-09-27 20:33:36 -0400

    File set-up for Assignment 2

--- experiments/conditionals/js/script.js
+++ experiments/conditionals/js/script.js
@@ -1,22 +1,16 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+Conditionals NOTES
+Rachel B Richard
 **************************************************/
 
 // setup()
-//
-// Description of setup() goes here.
 function setup() {
 
 }
 
 // draw()
-//
-// Description of draw() goes here.
 function draw() {
 
 }
