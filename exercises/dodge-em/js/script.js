de7a22050cbcf208e9bd5f924b2949683883152f
thisissherachel <rebr998@gmail.com>
2021-09-29 17:19:30 -0400

    Adding negativity object but troubleshoot part 2

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -23,7 +23,24 @@ let negativity = { //creating the negativity object
   vx: 0,
   vy: 0,
   speed: 20,
-  image: undefined,
+};
+
+let negativityTwo = { //creating another negativity object
+  x: 0,
+  y: 250,
+  size: 80,
+  vx: 0,
+  vy: 0,
+  speed: 20,
+};
+
+let negativityThree = { //creating another negativity object
+  x: 0,
+  y: 250,
+  size: 80,
+  vx: 0,
+  vy: 0,
+  speed: 20,
 };
 
 let user = { //creating the object for the interactive mouse
@@ -36,34 +53,21 @@ let user = { //creating the object for the interactive mouse
   ay: 0,
   acceleration: 1,
   maxSpeed: 20,
-  image: undefined,
-  image2: undefined,
+  currentImage: undefined,
+  happyImage: undefined,
+  sadImage: undefined,
+  caught: false,
 };
-
-function handleInput() { //settings for resetting the game
-  if (keyIsPressed(ENTER)) {
-    reset();
-  }
-}
-
-function reset() { //resetting all of properties to starting values
-  negativity.x = 0;
-  negativity.y = 250;
-  negativity.y = 0;
-
-  user.x = 250;
-  user.y = 250;
-  user.vx = 0;
-  user.ax = 1;
-}
 
 //PRELOAD
 // for images and asets going to be used in the running grogram
 function preload(){
   negativity.image = loadImage('assets/images/negativity.png');
-  user.image = loadImage('assets/images/happy.png');
-  user.image2 = loadImage('assets/images/sad.png');
+  user.happyImage = loadImage('assets/images/happy.png');
+  user.sadImage = loadImage('assets/images/sad.png');
 }
+
+
 
 // SETUP()
 //for full program running setup
@@ -74,24 +78,32 @@ function setup() {
   negativity.y = random(0, height); //moving at random y position
   negativity.vx = negativity.speed; //moving horizontally with its speeed
 
+  negativityTwo.y = random(0, height);
+  negativityTwo.vx = negativityTwo.speed;
+
+  negativityThree.y = random(0, height);
+  negativityThree.vx = negativityThree.speed;
+
   //removing cursor
   noCursor();
 }
+
 
 
 // DRAW()
 //for frame by frame program running
 function draw() {
   background(255,255,255);
+  handleInput()
 
-  //negativity display
+  //negativity displays
   imageMode(CENTER);
   image(negativity.image,negativity.x,negativity.y,100,100);
+  image(negativityTwo.image,negativityTwo.x,negativityTwo.y,100,100);
+  image(negativityThree.image,negativityThree.x,negativityThree.y,100,100);
 
   //user display
-
-  imageMode(CENTER);
-  image(user.image,user.x,user.y,100,100);
+  user.currentImage = user.happyImage;
 
   // movement of negativity
   negativity.x = negativity.x + negativity.speed;
@@ -99,6 +111,20 @@ function draw() {
   if (negativity.x > width || negativity.x < 0) { //keep it in the width and continue the random y position
     negativity.speed = -negativity.speed;
     negativity.y = random(0, height);
+  }
+
+  negativityTwo.x = negativityTwo.x + negativityTwo.speed;
+
+  if (negativityTwo.x > width || negativityTwo.x < 0) {
+    negativityTwo.speed = -negativityTwo.speed;
+    negativityTwo.y = random(0, height);
+  }
+
+  negativityThree.x = negativityThree.x + negativityThree.speed;
+
+  if (negativityThree.x > width || negativityThree.x < 0) {
+    negativityThree.speed = -negativityThree.speed;
+    negativityThree.y = random(0, height);
   }
 
   //user movement
@@ -125,10 +151,45 @@ function draw() {
   user.y = user.y + user.vy;
 
   //check for catching negativity
-  let d = dist(user.x,user.y,negativity.x,negativity.y); //finding the distance between user and negativity
-  if (d < negativity.size/2 + user.size/2) { //finding when they overlap
-    image(user.image2, user.x, user.y,100,100); 
-    user.image = user.image2;
+  let d = dist(user.x,user.y,negativity.x,negativity.y || user.x,user.y,negativityTwo.x,negativityTwo.y || user.x,user.y,negativityThree.x,negativityThree.y); //finding the distance between user and negativity
+  if (d < negativity.size/2 + user.size/2 || d < negativityTwo.size/2 + user.size/2 || d < negativityThree.size/2 + user.size/2 ) { //finding when they overlap
+    user.currentImage = user.sadImage;
+    user.caught = true;
     noLoop(); //stop the simulation once above is true
   }
+
+  //user display
+  imageMode(CENTER);
+  image(user.currentImage,user.x,user.y,100,100);
+
+  if (user.caught) {
+  noLoop();
+  }
+}
+
+
+
+function handleInput() { //settings for resetting the game
+  if (keyIsDown(ENTER)) {
+    reset();
+  }
+}
+
+function reset() { //resetting all of properties to starting values
+  negativity.x = 0;
+  negativity.y = 250;
+  negativity.y = 0;
+
+  negativityTwo.x = 0;
+  negativityTwo.y = 250;
+  negativityTwo.y = 0;
+
+  negativityThree.x = 0;
+  negativityThree.y = 250;
+  negativityThree.y = 0;
+
+  user.x = 250;
+  user.y = 250;
+  user.vx = 0;
+  user.ax = 1;
 }
