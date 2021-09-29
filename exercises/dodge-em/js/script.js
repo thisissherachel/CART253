aca43579f2f6fd0fb4202896c52df9777d9d50dd
thisissherachel <rebr998@gmail.com>
2021-09-29 16:32:42 -0400

    Mostly finished with the movement. Troubleshooting the images

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -6,7 +6,7 @@ Rachel B. Richard
 
 EXERCISE 02
 
-1. Change the way the user controls their circle
+1. Change the way the user controls their user
 2. Add at least one new if-statement (including at least an else)
 3. Change the way the simulation looks
 4. Use at least one image
@@ -16,29 +16,53 @@ The environment is happy with hearts until you touch object, it switches to dark
 
 **************************************************/
 
-let nagativity = { //creating the negativity object
+let negativity = { //creating the negativity object
   x: 0,
   y: 250,
-  size: 100,
+  size: 80,
   vx: 0,
   vy: 0,
-  speed: 5,
-  }
+  speed: 20,
+  image: undefined,
 };
 
 let user = { //creating the object for the interactive mouse
   x: 250,
   y: 250,
-  size:100,
-  fill: 100,
+  size: 80,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 1,
+  maxSpeed: 20,
+  image: undefined,
+  image2: undefined,
 };
 
+function handleInput() { //settings for resetting the game
+  if (keyIsPressed(ENTER)) {
+    reset();
+  }
+}
+
+function reset() { //resetting all of properties to starting values
+  negativity.x = 0;
+  negativity.y = 250;
+  negativity.y = 0;
+
+  user.x = 250;
+  user.y = 250;
+  user.vx = 0;
+  user.ax = 1;
+}
 
 //PRELOAD
-//for images and asets goign to be used in the running grogram
+// for images and asets going to be used in the running grogram
 function preload(){
-  negativity = loadImage('assets/images/negativity.jpg')
-  user = loadImage('assets/images/happy.jpg')
+  negativity.image = loadImage('assets/images/negativity.png');
+  user.image = loadImage('assets/images/happy.png');
+  user.image2 = loadImage('assets/images/sad.png');
 }
 
 // SETUP()
@@ -47,7 +71,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   //setting up the negativity settings
-  negativity.y = random(0,height); //moving at random y position
+  negativity.y = random(0, height); //moving at random y position
   negativity.vx = negativity.speed; //moving horizontally with its speeed
 
   //removing cursor
@@ -58,40 +82,53 @@ function setup() {
 // DRAW()
 //for frame by frame program running
 function draw() {
-  background(0);
+  background(255,255,255);
 
   //negativity display
   imageMode(CENTER);
-  image(negativity, width, height);
+  image(negativity.image,negativity.x,negativity.y,100,100);
 
   //user display
+
   imageMode(CENTER);
-  image(user, width, height);
+  image(user.image,user.x,user.y,100,100);
 
-  //negativity  movement
-  negativity.x += negativity.vx; //standard moving property
-  negativity.y += negativity.vy;
+  // movement of negativity
+  negativity.x = negativity.x + negativity.speed;
 
-  if (negativity.x > width) { //keep it in the width and continue the random y position
-    negativity.x = 0;
-    negativity.y = random(0,height);
+  if (negativity.x > width || negativity.x < 0) { //keep it in the width and continue the random y position
+    negativity.speed = -negativity.speed;
+    negativity.y = random(0, height);
   }
 
   //user movement
-  user.x = mouseX; //setting user position to the mouse position
-  user.y = mouseY;
-
-  //check for catching Covid19
-  let d = dist(user.x,user.y,negativity.x,negativity.y); //finding the distance between user and Covid19
-  if (d < negativity.size/2 + user.size/2) { //finding when they overlap
-    noLoop(); //stop the simulation once above is true
+  if (mouseX < user.x) {   //accelerating towards mouse as you move
+    user.ax = -user.acceleration;
+  }
+  else {
+    user.ax = user.acceleration;
   }
 
-  //display negativity
-  fill(negativity.fill.r,negativity.fill.g,negativity.fill.b);
-  ellipse(negativity.x,negativity.y,negativity.size);
+  if (mouseY < user.y) {
+    user.ay = -user.acceleration;
+  }
+  else {
+    user.ay = user.acceleration;
+  }
 
-  //display user
-  fill(user.fill);
-  ellipse(user.x,user.y,user.size);
+  user.vx = user.vx + user.ax; //updates users position with acceleration
+  user.vx = constrain(user.vx,-user.maxSpeed,user.maxSpeed); //limits the acceleration
+  user.vy = user.vy + user.ay;
+  user.vy = constrain(user.vy,-user.maxSpeed,user.maxSpeed);
+
+  user.x = user.x + user.vx; //updates users position with velocity
+  user.y = user.y + user.vy;
+
+  //check for catching negativity
+  let d = dist(user.x,user.y,negativity.x,negativity.y); //finding the distance between user and negativity
+  if (d < negativity.size/2 + user.size/2) { //finding when they overlap
+    image(user.image2, user.x, user.y,100,100); 
+    user.image = user.image2;
+    noLoop(); //stop the simulation once above is true
+  }
 }
