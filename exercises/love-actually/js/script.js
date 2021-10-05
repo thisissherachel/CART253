269fabe2e4b5aeb3ad3839a165935510ee1405bf
thisissherachel <rebr998@gmail.com>
2021-10-05 16:42:15 -0400

    E5: checkpoint 01
    -objects are moving correctly
    -controls are working
    -images are properly display
    *finalize GAME portion
    *figure out the frames to allow for the STATES to work properly
    *finalize backgrounds

--- exercises/love-actually/js/script.js
+++ exercises/love-actually/js/script.js
@@ -17,49 +17,57 @@ If you dont manage to catch it within the frames "aweeeee </3"
 
 **************************************************/
 
-let circle1 = {
-  x: undefined,
-  y: 250,
-  size: 100,
+let love = { //program controlled object
+  x: 100,
+  y: 100,
+  size: 200,
   vx: 0,
   vy: 0,
-  speed: 3
+  speed: 50,
 };
 
-let circle2 = {
-  x: undefined,
-  y: 250,
-  size: 100,
+let you = { //user controlled object
+  x: 500,
+  y: 500,
+  size: 200,
   vx: 0,
   vy: 0,
-  speed: 3
+  speed: 20,
+  currentImage: undefined,
+  lookingImage: undefined,
+  inloveImage: undefined,
+  darnImage: undefined,
+  gotit: false,
 };
 
-let state = `title`; // Can be: title, simulation, love, sadness
+let state = `title`; // Can be: title, simulation, yay, wow, awe
 
-function setupCircles() {
-  // Position circles separated from one another
-  circle1.x = width / 3;
-  circle2.x = 2 * width / 3;
-
-  // Start circles moving in a random direction
-  circle1.vx = random(-circle1.speed,circle1.speed);
-  circle1.vy = random(-circle1.speed,circle1.speed);
-  circle2.vx = random(-circle2.speed,circle2.speed);
-  circle2.vy = random(-circle2.speed,circle2.speed);
+//PRELOAD
+// for images and asets going to be used in the running grogram
+function preload(){
+  love.image = loadImage('assets/images/heart.png');
+  you.searchingImage = loadImage('assets/images/eyes.png');
+  you.inloveImage = loadImage('assets/images/inlove.png');
+  you.darnImage = loadImage('assets/images/darn.png');
 }
 
-// setup()
+
+//SETUP
 //for full program running setup
 function setup() {
-  createCanvas(500,500);
-  setupCircles();
+  createCanvas(windowWidth,windowHeight);
+
+  // //setting up love object
+  // catchMe();
+
+  // //removing cursor
+  // noCursor();
 }
 
-// draw()
+//DRAW
 //for frame by frame program running
 function draw() {
-  background(0);
+  background(255);
 
    if (state === `title`) {
      title();
@@ -67,89 +75,158 @@ function draw() {
    else if (state === `simulation`) {
      simulation();
    }
-   else if (state === `love`) {
-     love();
+   else if (state === `wow`) {
+     fastinlove();
    }
-   else if (state === `sadness`) {
-     sadness();
+   else if (state === `yay`) {
+     inlove();
+   }
+   else if (state === `awe`) {
+     nolove();
    }
 }
 
+
+//STATES
 function title() {
   push();
-  textSize(64);
+  textSize(69);
   fill(200,100,100);
   textAlign(CENTER,CENTER);
-  text(`LOVE?`,width/2,height/2);
+  textStyle(ITALIC);
+  text(`catch some love baby!`,width/2,height/2);
+  pop();
+
+  push();
+  textSize(20);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text(`(use arrow keys to move)`,width/2,2*height/3);
   pop();
 }
 
 function simulation() {
-  move();
-  checkOffscreen();
-  checkOverlap();
-  display();
+  movementLove();
+  displayLove();
+  movementYou();
+  displayYou();
+  game();
 }
 
-function love() {
+function fastinlove() {
   push();
-  textSize(64);
+  textSize(69);
   fill(255,150,150);
   textAlign(CENTER,CENTER);
-  text(`LOVE!`,width/2,height/2);
+  text(`wow that was fast!`,width/2,height/2);
   pop();
 }
 
-function sadness() {
+function inlove() {
   push();
-  textSize(64);
+  textSize(69);
+  fill(255,150,150);
+  textAlign(CENTER,CENTER);
+  text(`YAY <3`,width/2,height/2);
+  pop();
+}
+
+function nolove() {
+  push();
+  textSize(69);
   fill(150,150,255);
   textAlign(CENTER,CENTER);
-  text(`:(`,width/2,height/2);
+  text(`awe </3`,width/2,height/2);
   pop();
 }
 
-function move() {
-  // Move the circles
-  circle1.x = circle1.x + circle1.vx;
-  circle1.y = circle1.y + circle1.vy;
 
-  circle2.x = circle2.x + circle2.vx;
-  circle2.y = circle2.y + circle2.vy;
-}
-
-function checkOffscreen() {
-  // Check if the circles have gone offscreen
-  if (isOffscreen(circle1) || isOffscreen(circle2)) {
-    state = `sadness`;
-  }
-}
-
-function isOffscreen(circle) {
-  if (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
-function checkOverlap() {
-  // Check if the circles overlap
-  let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
-  if (d < circle1.size/2 + circle2.size/2) {
-    state = `love`;
-  }
-}
-
-function display() {
-  // Display the circles
-  ellipse(circle1.x,circle1.y,circle1.size);
-  ellipse(circle2.x,circle2.y,circle2.size);
-}
-
+//CONTROLS FOR START
 function mousePressed() {
   if (state === `title`) {
     state = `simulation`;
+  }
+}
+
+
+//LOVE DISPLAY/MOVEMENT
+function movementLove() {
+  //making love randomly move around the canvas
+  let change = random();
+  if (change < 0.025) { //5% chances of the time the love will move
+    love.vx = random(-love.speed,love.speed);
+    love.vy = random(-love.speed,love.speed);
+  }
+  //constrain to convas
+  love.x = constrain(love.x,100,windowWidth-100);
+  love.y = constrain(love.y,100,windowHeight-100);
+  //movement
+  love.x += love.vx;
+  love.y += love.vy;
+}
+
+function displayLove() {
+  //love displayed with image
+  imageMode(CENTER);
+  image(love.image,love.x,love.y,200,200);
+}
+
+
+//YOU DISPLAY/MOVEMENT
+function movementYou() {
+  handleInput()
+  //constrain to convas
+  you.x = constrain(you.x,100,windowWidth-100);
+  you.y = constrain(you.y,100,windowHeight-100);
+  //movement
+  you.x += you.vx;
+  you.y += you.vy;
+}
+
+function displayYou() {
+  //user displayed with image
+  you.currentImage = you.searchingImage;
+  imageMode(CENTER);
+  image(you.searchingImage,you.x,you.y,200,200);
+}
+
+
+//USER CONTROLS
+function handleInput() {
+  //user controls for the eyes
+  if (keyIsDown(LEFT_ARROW)) {
+    you.vx = -you.speed;
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    you.vx = you.speed;
+  }
+  else { //if neither of the above are true
+    you.vx = 0;
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    you.vy = -you.speed;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    you.vy = you.speed;
+  }
+  else { //if neither of the above are true
+    you.vy = 0;
+  }
+}
+
+
+//GAME
+function game() {
+  //check for catching love
+  let d = dist(you.x, you.y, love.x, love.y); //finding the distance between user and love
+  if (d < love.size / 2 + you.size / 2) { //finding when they overlap
+    you.gotit = true;
+  }
+  //stopping when true
+  if (you.gotit) {
+    love.speed = 0;
+    inlove(); //display state
+    you.currentImage = you.inloveImage; //change image
   }
 }
