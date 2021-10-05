f1317a223f72724e5296404c6cfa79242d900891
thisissherachel <rebr998@gmail.com>
2021-10-05 13:42:26 -0400

    A5: file setup for activity 05

---
+++ activities/looking-for-love/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**************************************************
+Looking for Love
+Rachel B. Richard
+
+ACTIVITY 05
+1. Use states to manage your program
+2. Use functions to organize your program
+3. Write a function with a parameter and return value
+**************************************************/
+
+// setup()
+//for full program running setup
+function setup() {
+
+}
+
+// draw()
+//for frame by frame program running
+function draw() {
+
+}
