1772dfd12228a0755294f4ee54ee03f6365111c7
thisissherachel <rebr998@gmail.com>
2021-10-08 15:25:17 -0400

    6.2 "use strict" notes

--- experiments/sound-and-strict/js/script.js
+++ experiments/sound-and-strict/js/script.js
@@ -1,4 +1,3 @@
-"use strict";
 
 /**************************************************
 Sounf and Strict NOTES
@@ -10,11 +9,50 @@ Rachel B. Richard
 //need sound library for sound in java (p5.sound.min.js)
 //sound will only play AFTER user nteraction so it cannot be implemented in setup()
 
-let music;
+// let music;
+//
+// // preload()
+// function preload(){
+//   music = loadSound(`assets/sounds/bark.wav`);
+// }
+//
+// // setup()
+// function setup() {
+//   createCanvas(500,500);
+// }
+//
+// // draw()
+// function draw() {
+//   background(0);
+// }
+//
+// // event handlers
+// function mousePressed() {
+//   // music.play(); //plays the sound everytime the mouse is pressed (different files play on different browsers - mp3 is safest)
+//   tryMusic();
+// }
+//
+// function keyPressed() {
+//   tryMusic();
+// }
+//
+// function tryMusic() {
+//   if (!music.isPlaying()) { //checks if music is playing and if it is dont play it again
+//     music.loop(); //loops sound
+//   }
+// }
 
-// preload()
-function preload(){
-  music = loadSound(`assets/sounds/bark.wav`);
+//6.2 "use strict"
+
+"use strict";
+//very first line of every program
+//program still would with no errors in console but it doesnt do what it needs to do.
+//program assumes what you want and creates variables for you which isnt always correct.
+
+let circle = {
+  x: 0,
+  y: 0,
+  size: 100,
 }
 
 // setup()
@@ -25,20 +63,7 @@ function setup() {
 // draw()
 function draw() {
   background(0);
-}
-
-// event handlers
-function mousePressed() {
-  // music.play(); //plays the sound everytime the mouse is pressed (different files play on different browsers - mp3 is safest)
-  tryMusic();
-}
-
-function keyPressed() {
-  tryMusic();
-}
-
-function tryMusic() {
-  if (!music.isPlaying()) { //checks if music is playing and if it is dont play it again
-    music.loop(); //loops sound
-  }
+  circle.x = mouseX;
+  circley = mouseY; //mistake that the program will assume you want this to create the variable.
+  ellipse(circle.x,mouse.y,100);
 }
