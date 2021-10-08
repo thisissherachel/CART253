097ae7ee178a0b84d6d35d567d1e167135f03a07
thisissherachel <rebr998@gmail.com>
2021-10-08 15:18:53 -0400

    6.1 sound notes

--- experiments/sound-and-strict/js/script.js
+++ experiments/sound-and-strict/js/script.js
@@ -7,12 +7,38 @@ Rachel B. Richard
 
 //6.1 Sound
 
+//need sound library for sound in java (p5.sound.min.js)
+//sound will only play AFTER user nteraction so it cannot be implemented in setup()
+
+let music;
+
+// preload()
+function preload(){
+  music = loadSound(`assets/sounds/bark.wav`);
+}
+
 // setup()
 function setup() {
-
+  createCanvas(500,500);
 }
 
 // draw()
 function draw() {
+  background(0);
+}
 
+// event handlers
+function mousePressed() {
+  // music.play(); //plays the sound everytime the mouse is pressed (different files play on different browsers - mp3 is safest)
+  tryMusic();
+}
+
+function keyPressed() {
+  tryMusic();
+}
+
+function tryMusic() {
+  if (!music.isPlaying()) { //checks if music is playing and if it is dont play it again
+    music.loop(); //loops sound
+  }
 }
