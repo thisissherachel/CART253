d48f62498082084a7ac64c81ee1e8b6e216661e9
thisissherachel <rebr998@gmail.com>
2021-10-08 15:00:48 -0400

    E6: files setup for sound and strict notes

---
+++ experiments/sound-and-strict/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**************************************************
+Sounf and Strict NOTES
+Rachel B. Richard
+**************************************************/
+
+//6.1 Sound
+
+// setup()
+function setup() {
+
+}
+
+// draw()
+function draw() {
+
+}
