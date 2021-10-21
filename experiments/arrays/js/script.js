5e9638d95e0bc5c1f03db8cde384155d5380dd6e
thisissherachel <rebr998@gmail.com>
2021-10-20 21:04:42 -0400

    E6: arrays setup

---
+++ experiments/arrays/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**************************************************
+Arrays NOTES
+Rachel B. Richard
+**************************************************/
+
+// setup()
+//for full program running setup
+function setup() {
+
+}
+
+// draw()
+//for frame by frame program running
+function draw() {
+
+}
