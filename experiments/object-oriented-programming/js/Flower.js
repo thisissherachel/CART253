564b30a1740d7f01c202c96438a585a92067445d
thisissherachel <rebr998@gmail.com>
2021-10-26 10:41:05 -0400

    8.2 Create a class for objects with different properties

--- experiments/object-oriented-programming/js/Flower.js
+++ experiments/object-oriented-programming/js/Flower.js
@@ -45,3 +45,43 @@ class Flower {
 //   }
 // }
 
+//8.2
+//assigning variables
+//constructor allows you to make properties to an object called this.
+  constructor(x, y, size, stemLength, petalColor) {
+    // Position and size information
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.stemLength = stemLength;
+    this.stemThickness = 10;
+    this.petalThickness = 10;
+    // Color information
+    this.stemColor = {
+      r: 50,
+      g: 150,
+      b: 50
+    };
+    this.petalColor = petalColor
+    this.centreColor = {
+      r: 50,
+      g: 0,
+      b: 0
+    }
+  }
+
+//display allows you to draw an object called this.
+  display() {
+    push();
+    // Draw a line for the stem
+    strokeWeight(this.stemThickness);
+    stroke(this.stemColor.r, this.stemColor.g, this.stemColor.b);
+    line(this.x, this.y, this.x, this.y + this.stemLength);
+    // Draw a circle with a heavy outline for the flower
+    strokeWeight(this.petalThickness);
+    fill(this.centreColor.r, this.centreColor.g, this.centreColor.b);
+    stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
+    ellipse(this.x, this.y, this.size);
+    pop();
+  }
+}
--- experiments/object-oriented-programming/js/script.js
+++ experiments/object-oriented-programming/js/script.js
@@ -48,8 +48,54 @@ Rachel B. Richard
 //   }
 // }
 
-}
-
-function draw() {
-
-}
+// //8.2 Constructors with parameters
+// //how to use a class to make an object with different forms
+//
+// // Our garden
+// let garden = {
+//   // An array to store the individual flowers
+//   flowers: [],
+//   // How many flowers in the garden
+//   numFlowers: 20,
+//   // The color of the grass (background)
+//   grassColor: {
+//     r: 120,
+//     g: 180,
+//     b: 120
+//   }
+// };
+//
+// // setup() creates the canvas and the flowers in the garden
+// function setup() {
+//   createCanvas(600, 600);
+//
+//   // Create our flowers by counting up to the number of the flowers
+//   for (let i = 0; i < garden.numFlowers; i++) {
+//     let x = random(0, width);
+//     let y = random(0, height);
+//     let size = random(50, 80);
+//     let stemLength = random(50, 100);
+//     let petalColor = {
+//       r: random(100, 255),
+//       g: random(100, 255),
+//       b: random(100, 255)
+//     };
+//     // Create a new flower
+//     let flower = new Flower(x, y, size, stemLength, petalColor); //!!how to call a class constructor
+//     // Add the flower to the array of flowers
+//     garden.flowers.push(flower);
+//   }
+// }
+//
+// // draw()
+// // Displays our flowers
+// function draw() {
+//   // Display the grass
+//   background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+//
+//   // Loop through all the flowers in the array and display them
+//   for (let i = 0; i < garden.flowers.length; i++) {
+//     let flower = garden.flowers[i];
+//     flower.display(); //!! how to call a class display
+//   }
+// }
