34f6f3872d9ca2824889dc898a441bdf971a743d
thisissherachel <rebr998@gmail.com>
2021-10-26 11:39:12 -0400

    ACTIVITY: Object Oriented Activity
    file set up

---
+++ activities/object-oriented-activity/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**************************************************
+Object Oriented Activity
+Rachel B. Richard
+
+ACTIVITY
+
+Make a juggling simulation!
+
+The user will control a rectangular “paddle” at the bottom of the screen, moving it horizontally with the mouse position.
+Balls will fall from the top of the screen. If they hit the paddle, they will bounce upward and fall again. If they miss the paddle, they will disappear off the bottom.
+**************************************************/
+
+// setup()
+//for full program running setup
+function setup() {
+
+}
+
+// draw()
+//for frame by frame program running
+function draw() {
+
+}
