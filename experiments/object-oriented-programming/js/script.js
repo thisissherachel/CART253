cf81f511001ad79fca37a2379252d4b2913cfe2e
thisissherachel <rebr998@gmail.com>
2021-10-26 10:40:30 -0400

    8.1 How to create classes for objects

--- experiments/object-oriented-programming/js/script.js
+++ experiments/object-oriented-programming/js/script.js
@@ -5,8 +5,48 @@ Object Oriented Programmming NOTES
 Rachel B. Richard
 **************************************************/
 
+//8.1 Introducing Object Oriented Programming
+//create a program using class to tidy away your objects
 
-function setup() {
+// Our garden
+// let garden = {
+//   // An array to store the individual flowers
+//   flowers: [],
+//   // How many flowers in the garden
+//   numFlowers: 20,
+//   // The color of the grass (background)
+//   grassColor: {
+//     r: 120,
+//     g: 180,
+//     b: 120
+//   }
+// };
+//
+// // setup() creates the canvas and the flowers in the garden
+// function setup() {
+//   createCanvas(600, 600);
+//
+//   // Create our flowers by counting up to the number of the flowers
+//   for (let i = 0; i < garden.numFlowers; i++) {
+//     // Create a new flower
+//     let flower = new Flower(); //!!how to call a class constructor
+//     // Add the flower to the array of flowers
+//     garden.flowers.push(flower);
+//   }
+// }
+//
+// // draw()
+// // Displays our flowers
+// function draw() {
+//   // Display the grass
+//   background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+//
+//   // Loop through all the flowers in the array and display them
+//   for (let i = 0; i < garden.flowers.length; i++) {
+//     let flower = garden.flowers[i];
+//     flower.display(); //!! how to call a class display
+//   }
+// }
 
 }
 
