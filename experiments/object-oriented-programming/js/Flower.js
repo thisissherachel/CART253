23cbcc78840172e1d81bba715f875f62f0f5d618
thisissherachel <rebr998@gmail.com>
2021-10-26 11:34:13 -0400

    8.4 Having objects interact with one another through a class

--- experiments/object-oriented-programming/js/Flower.js
+++ experiments/object-oriented-programming/js/Flower.js
@@ -1,6 +1,8 @@
 class Flower {
 
 // //8.1
+// //creating a class
+
 // //constructor allows you to make properties to an object called this.
 //   constructor() {
 //     // Position and size information
@@ -43,10 +45,11 @@ class Flower {
 //     ellipse(this.x, this.y, this.size);
 //     pop();
 //   }
-// }
+
 
 // //8.2
 // //assigning variables
+
 // //constructor allows you to make properties to an object called this.
 //   constructor(x, y, size, stemLength, petalColor) {
 //     // Position and size information
@@ -84,19 +87,78 @@ class Flower {
 //     ellipse(this.x, this.y, this.size);
 //     pop();
 //   }
-// }
 
-//8.3
-//assigning events to a class
+
+// //8.3
+// //assigning events to a class
+
+// //constructor allows you to make properties to an object called this.
+//   constructor(x, y, size, stemLength, petalColor) {
+//     // Position and size information
+//     this.x = x;
+//     this.y = y;
+//     this.size = size;
+//     this.stemLength = stemLength;
+//     this.stemThickness = 10;
+//     this.petalThickness = 10;
+//     // Color information
+//     this.stemColor = {
+//       r: 50,
+//       g: 150,
+//       b: 50
+//     };
+//     this.petalColor = petalColor
+//     this.centreColor = {
+//       r: 50,
+//       g: 0,
+//       b: 0
+//     };
+//   }
+//
+// //display allows you to draw an object called this.
+//   display() {
+//     push();
+//     // Draw a line for the stem
+//     strokeWeight(this.stemThickness);
+//     stroke(this.stemColor.r, this.stemColor.g, this.stemColor.b);
+//     line(this.x, this.y, this.x, this.y + this.stemLength);
+//     // Draw a circle with a heavy outline for the flower
+//     strokeWeight(this.petalThickness);
+//     fill(this.centreColor.r, this.centreColor.g, this.centreColor.b);
+//     stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
+//     ellipse(this.x, this.y, this.size);
+//     pop();
+//   }
+//
+// //adding an event
+//   mousePressed() {
+//     // Calculate the distance between this flower and the mouse
+//     let d = dist(this.x,this.y,mouseX,mouseY);
+//     // Check if the distance is less than the head of the flower
+//     if (d < this.size/2 + this.petalThickness) {
+//       // If it is, this flower was clicked, so increase its stem length
+//       this.stemLength = this.stemLength + 5;
+//       // And also change its y position so it grows upward! (If we didn't do this
+//       // the then stem would grow downward, which would look weird.)
+//       this.y = this.y - 5;
+//     }
+//   }
+
+
+//8.4
+//adding interactivity to a class
+
 //constructor allows you to make properties to an object called this.
   constructor(x, y, size, stemLength, petalColor) {
     // Position and size information
     this.x = x;
     this.y = y;
     this.size = size;
+    this.maxSize = size; //!! To limit growth
     this.stemLength = stemLength;
     this.stemThickness = 10;
     this.petalThickness = 10;
+    this.maxPetalThickness = 10; //!! To limit growth
     // Color information
     this.stemColor = {
       r: 50,
@@ -109,7 +171,37 @@ class Flower {
       g: 0,
       b: 0
     };
+    this.alive = true;
   }
+
+//!! adding an interactive element
+  shrink() {
+  // Choose a random amount to shrink
+  let shrinkage = random(0, 0.1);
+  // Reduce the petal thickness (divide by 10 to make it less rapid)
+  this.petalThickness = this.petalThickness - shrinkage / 10;
+  // Reduce the centre of the flower
+  this.size = this.size - shrinkage;
+
+  // If any of the key properties reach 0 or less, the flower is dead
+  if (this.petalThickness <= 0 || this.size <= 0) {
+    this.alive = false;
+  }
+}
+
+//!! pollinate() handles the flower being pollinated (it grows)
+pollinate() {
+  // Choose a random amount to grow
+  let growth = random(0, 0.5);
+  // Increase the petal thickness (divide by 10 to make it less rapid)
+  this.petalThickness = this.petalThickness + growth / 10;
+  // Increase the centre of the flower
+  this.size = this.size + growth;
+
+  // Constrain the elements
+  this.petalThickness = constrain(this.petalThickness, 0, this.maxPetalThickness);
+  this.size = constrain(this.size, 0, this.maxSize);
+}
 
 //display allows you to draw an object called this.
   display() {
@@ -139,4 +231,5 @@ class Flower {
       this.y = this.y - 5;
     }
   }
+
 }
--- experiments/object-oriented-programming/js/script.js
+++ experiments/object-oriented-programming/js/script.js
@@ -100,8 +100,70 @@ Rachel B. Richard
 //   }
 // }
 
-//8.3 object oriented Programming and p5 events
-//adding events to a class
+// //8.3 object oriented Programming and p5 events
+// //adding events to a class
+//
+// // Our garden
+// let garden = {
+//   // An array to store the individual flowers
+//   flowers: [],
+//   // How many flowers in the garden
+//   numFlowers: 20,
+//   // The color of the grass (background)
+//   grassColor: {
+//     r: 120,
+//     g: 180,
+//     b: 120
+//   }
+// };
+//
+// // setup() creates the canvas and the flowers in the garden
+// function setup() {
+//   createCanvas(600, 600);
+//
+//   // Create our flowers by counting up to the number of the flowers
+//   for (let i = 0; i < garden.numFlowers; i++) {
+//     let x = random(0, width);
+//     let y = random(0, height);
+//     let size = random(50, 80);
+//     let stemLength = random(50, 100);
+//     let petalColor = {
+//       r: random(100, 255),
+//       g: random(100, 255),
+//       b: random(100, 255)
+//     };
+//     // Create a new flower
+//     let flower = new Flower(x, y, size, stemLength, petalColor); //!! how to call a assigned properties in class constructor
+//     // Add the flower to the array of flowers
+//     garden.flowers.push(flower);
+//   }
+// }
+//
+// // draw()
+// // Displays our flowers
+// function draw() {
+//   // Display the grass
+//   background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+//
+//   // Loop through all the flowers in the array and display them
+//   for (let i = 0; i < garden.flowers.length; i++) {
+//     let flower = garden.flowers[i];
+//     flower.display(); //!! how to call a class display
+//   }
+// }
+//
+// //!! create a functio to call the class event
+// function mousePressed() {
+//   // Loop through all the flowers in the array to check if it was pressed
+//   for (let i = 0; i < garden.flowers.length; i++) {
+//     let flower = garden.flowers[i];
+//     flower.mousePressed(); //!! how to call a event in a class
+//   }
+// }
+
+
+//8.4 Interactive objects
+//adding interactive elements to objects through a class
 
 // Our garden
 let garden = {
@@ -109,6 +171,10 @@ let garden = {
   flowers: [],
   // How many flowers in the garden
   numFlowers: 20,
+  // An array to our the bees
+  bees: [],
+  // How many bees in the garden
+  numBees: 10,
   // The color of the grass (background)
   grassColor: {
     r: 120,
@@ -123,6 +189,7 @@ function setup() {
 
   // Create our flowers by counting up to the number of the flowers
   for (let i = 0; i < garden.numFlowers; i++) {
+    // Create variables for our arguments for clarity
     let x = random(0, width);
     let y = random(0, height);
     let size = random(50, 80);
@@ -131,12 +198,24 @@ function setup() {
       r: random(100, 255),
       g: random(100, 255),
       b: random(100, 255)
-    };
-    // Create a new flower
-    let flower = new Flower(x, y, size, stemLength, petalColor); //!! how to call a assigned properties in class constructor
+    }
+    // Create a new flower using the arguments
+    let flower = new Flower(x, y, size, stemLength, petalColor);
     // Add the flower to the array of flowers
     garden.flowers.push(flower);
   }
+
+  // Create our bees by counting up to the number of bees
+  for (let i = 0; i < garden.numBees; i++) {
+    // Create variables for our arguments for clarity
+    let x = random(0, width);
+    let y = random(0, height);
+    // Create a new bee using the arguments
+    let bee = new Bee(x, y);
+    // Add the bee to the array of bees
+    garden.bees.push(bee);
+  }
+
 }
 
 // draw()
@@ -148,15 +227,34 @@ function draw() {
   // Loop through all the flowers in the array and display them
   for (let i = 0; i < garden.flowers.length; i++) {
     let flower = garden.flowers[i];
-    flower.display(); //!! how to call a class display
+    // Check if this flower is alive
+    if (flower.alive) {
+      // Update the flower by shrinking it and displaying it
+      flower.shrink();
+      flower.display();
+    }
   }
-}
 
-//!! create a functio to call the class event
-function mousePressed() {
-  // Loop through all the flowers in the array to check if it was pressed
-  for (let i = 0; i < garden.flowers.length; i++) {
-    let flower = garden.flowers[i];
-    flower.mousePressed(); //!! how to call a event in a class
+  // Loop through all the bees in the array and display them
+  for (let i = 0; i < garden.bees.length; i++) {
+    let bee = garden.bees[i];
+    // Check if this flower is alive
+    if (bee.alive) {
+      // Shrink and move the bee
+      bee.shrink();
+      bee.move();
+
+      //!! Go through the entire flower array and try to pollinate the flowers!
+      // Note that we use j in our for-loop here because we're already inside
+      // a for-loop using i!
+      for (let j = 0; j < garden.flowers.length; j++) {
+        let flower = garden.flowers[j];
+        bee.tryToPollinate(flower);
+      }
+
+      // Display the bee
+      bee.display();
+    }
   }
+
 }
