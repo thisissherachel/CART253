f813f10baae7f85491ba9c41fb292471685f64b2
thisissherachel <rebr998@gmail.com>
2021-11-02 12:23:23 -0400

    A: Inheritance Activity
    - file setup

---
+++ activities/inheritance-activity/js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**************************************************
+Inheritance Activity
+Rachel B. Richard
+
+ACTIVITY
+
+Here is a suggested plan:
+
+1. Write a main script with states
+2. Create a basic Pedestrian class and integrate it into the main script
+3. Create a Vehicle class
+4. Create Car, Truck, and Motorcycle classes that extend the Vehicle class
+5. Add cars, trucks, and motorcycles to the main script
+6. Add success and failure conditions
+
+The user will control a circular pedestrian at the bottom of the canvas. Cars, trucks, and motorcycles will be moving left and right across the canvas.
+If the pedestrian collides with any of the vehicles, they die. If they make it to the other side, they succeeded. Just like life!
+**************************************************/
