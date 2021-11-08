1bb071d216c00807c3002b6752fa3a705faca5de
thisissherachel <rebr998@gmail.com>
2021-11-08 15:50:13 -0500

    Project 2: file set up

---
+++ exercises/project-2/README.md
@@ -0,0 +1,3 @@
+# Just have fun
+
+Plan for this program to include and hone in on all projects I work on this semester.
---
+++ exercises/project-2/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**************************************************
+Just have fun.
+Rachel B. Richard
+
+PROJECT 02
+
+**************************************************/
+
+
+//PRELOAD
+// for images and assets going to be used in the running grogram
+function preload() {
+
+}
+
+
+//SETUP
+//for full program running setup
+function setup() {
+
+}
+
+//DRAW
+//for frame by frame program running setup
+function draw() {
+
+}
