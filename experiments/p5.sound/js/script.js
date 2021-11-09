a59df9fc81e0669db416f5f2cbffa770a053f194
thisissherachel <rebr998@gmail.com>
2021-11-09 11:06:08 -0500

    10.4 p5.AudioIn
    - interactivity with a microphone

--- experiments/p5.sound/js/script.js
+++ experiments/p5.sound/js/script.js
@@ -72,56 +72,133 @@ Rachel B. Richard
 //   siren.start();
 // }
 
-//10.3 PolySynth
-//creating a synthesizer
+// //10.3 PolySynth
+// //creating a synthesizer
+//
+// // Our synthesizer
+// let synth;
+// // musical notes ("b" means "flat" if you haven't seen it before)
+// let notes = [`F2`, `G2`, `Ab3`, `Bb3`, `C4`, `Db4`, `Eb4`, `F5`];
+// // The current note to play, start at the beginning
+// let currentNote = 0;
+// // To track the interval that plays note
+// let interval;
+//
+// function setup() {
+//   createCanvas(600, 600);
+//   userStartAudio();
+//
+//   // Create the synthesizer
+//   synth = new p5.PolySynth();
+// }
+//
+// function draw() {
+//   background(0);
+// }
+//
+// // mousePressed() starts and stops our piano playing
+// function mousePressed() {
+//   // First check that the piano isn't already playing
+//   // The interval will be undefined if it hasn't started
+//   if (interval === undefined) {
+//     // Start our interval, calling playNextNote every 500 milliseconds
+//     // Assign the result to interval to remember the interval
+//     interval = setInterval(playNextNote, 500);
+//   }
+//   else {
+//     // If they click when it's playing, clear the interval and set interval
+//     // back to undefined to stop play
+//     clearInterval(interval);
+//     interval = undefined;
+//   }
+// }
+//
+// // playNextNote() plays the next note in our array
+// function playNextNote() {
+//   // Chose the note at the current position
+//   let note = notes[currentNote];
+//   // Play it
+//   synth.play(note, 0.2, 0, 0.4);
+//   // Increase the current position and go back to 0 when we reach the end
+//   currentNote = currentNote + 1;
+//   if (currentNote === notes.length) {
+//     currentNote = 0;
+//   }
+// }
 
-// Our synthesizer
-let synth;
-// musical notes ("b" means "flat" if you haven't seen it before)
-let notes = [`F2`, `G2`, `Ab3`, `Bb3`, `C4`, `Db4`, `Eb4`, `F5`];
-// The current note to play, start at the beginning
-let currentNote = 0;
-// To track the interval that plays note
-let interval;
+//10.4 p5.AudioIn
+//how to make a program interactive with sound
+
+// The microphone
+let mic;
+// A ghost
+let ghost;
+// A clown image
+let clownImage;
+
+function preload() {
+  clownImage = loadImage(`assets/images/clown.png`);
+}
 
 function setup() {
   createCanvas(600, 600);
-  userStartAudio();
 
-  // Create the synthesizer
-  synth = new p5.PolySynth();
+  // Create our AudioIn object
+  mic = new p5.AudioIn();
+  // Try to connect to the user's microphone
+  mic.start();
+  // Create our ghost
+  ghost = createGhost(clownImage);
+}
+
+// Create a ghost in the center of the screen with various
+// ghostly properties
+function createGhost(ghostImage) {
+  let ghost = {
+    x: width / 2,
+    y: height / 2,
+    vx: 0,
+    vy: 0,
+    happySpeed: 1, // How fast the ghost moves when happy
+    scaredSpeed: 25, // How fast the ghost moves when scared
+    image: ghostImage, // The image to display
+    alpha: 50, // How transparent is the ghost,
+    state: `happy`, // How does the ghost feel right now?,
+    scaredThreshold: 0.2 // How loud a sound makes the ghost afraid?
+  };
+  return ghost;
 }
 
 function draw() {
   background(0);
-}
 
-// mousePressed() starts and stops our piano playing
-function mousePressed() {
-  // First check that the piano isn't already playing
-  // The interval will be undefined if it hasn't started
-  if (interval === undefined) {
-    // Start our interval, calling playNextNote every 500 milliseconds
-    // Assign the result to interval to remember the interval
-    interval = setInterval(playNextNote, 500);
-  }
-  else {
-    // If they click when it's playing, clear the interval and set interval
-    // back to undefined to stop play
-    clearInterval(interval);
-    interval = undefined;
-  }
-}
+  // Get the current level of sound going into the microphone
+  let level = mic.getLevel();
 
-// playNextNote() plays the next note in our array
-function playNextNote() {
-  // Chose the note at the current position
-  let note = notes[currentNote];
-  // Play it
-  synth.play(note, 0.2, 0, 0.4);
-  // Increase the current position and go back to 0 when we reach the end
-  currentNote = currentNote + 1;
-  if (currentNote === notes.length) {
-    currentNote = 0;
+  // Check if the ghost gets scared
+  if (level > ghost.scaredThreshold) {
+    ghost.state = `afraid`;
+    // The ghost should run away to the right
+    ghost.vx = ghost.scaredSpeed;
   }
+
+  // Check if the ghost is happy at the moment
+  if (ghost.state === `happy`) {
+    // If the ghost is happy, it moves around randomly
+    let r = random(0, 1);
+    if (r < 0.1) {
+      ghost.vx = random(-ghost.happySpeed, ghost.happySpeed);
+      ghost.vy = random(-ghost.happySpeed, ghost.happySpeed);
+    }
+  }
+
+  // Move the ghost
+  ghost.x += ghost.vx;
+  ghost.y += ghost.vy;
+
+  // Display the ghost
+  push();
+  tint(255, ghost.alpha);
+  image(ghost.image, ghost.x, ghost.y);
+  pop();
 }
