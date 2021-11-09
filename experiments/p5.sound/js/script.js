2a3b4860d854fa6ad66b112f32dd7197ade005af
thisissherachel <rebr998@gmail.com>
2021-11-09 10:09:25 -0500

    10.1 Exploring the API for sound
    - the use of rate in sound

--- experiments/p5.sound/js/script.js
+++ experiments/p5.sound/js/script.js
@@ -5,11 +5,33 @@ p5.sound NOTES
 Rachel B. Richard
 **************************************************/
 
+//10.1 Reintroducing p5 sound
+//exploring the API for sound (p5 reference) : rate
+
+let barkSFX;
+
+function preload() {
+  barkSFX = loadSound(`assets/sounds/bark.wav`);
+}
 
 function setup() {
-
+  createCanvas(600, 600);
+  userStartAudio();
 }
 
 function draw() {
+  background(0);
 
+  // We can calculate the rate we should set the sound to by mapping
+  // the mouse's x position to our desired range..
+  let newRate = map(mouseX, 0, width, -3, 3);
+  // And then set the rate of our sound file to the new rate
+  // Note how we can do this while the sound is still playing!
+  barkSFX.rate(newRate);
+}
+
+function mousePressed() {
+  // Since we'll be playing around with the sound, it makes sense to loop it
+  // so it doesn't stop.
+  barkSFX.loop();
 }
